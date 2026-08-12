24219f1f70252a48b2f9808519a50dc0cd0c1594
ianmtz07 <ianmtz07@outlook.com>
2026-08-11 23:13:40 -0500

    Wire branded eBay listing preview into app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ListingTemplatePreviewBridge } from './components/ListingTemplatePreviewBridge'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ListingTemplatePreviewBridge />
   </StrictMode>,
 )
