ea9a64a607e89eb27eda079fc8572db9f215a9ad
ianmtz07 <ianmtz07@outlook.com>
2026-09-24 05:05:54 +0000

    Test pass one whitening halo source

--- src/utils/texasOEMPhotoV2.ts
+++ src/utils/texasOEMPhotoV2.ts
@@ -230,8 +230,12 @@ export async function createTexasOEMPhotoV2(
        * It should attack dirty/yellow seams while the
        * dark amplifier receives little or no whitening.
        */
-      const whiteStrength =
-        backgroundConfidence * 0.68
+      /*
+       * DIAGNOSTIC:
+       * Disable Pass 1 whitening to verify whether this
+       * stage is creating the product-edge gray halo.
+       */
+      const whiteStrength = 0
 
       r += (250 - r) * whiteStrength
       g += (250 - g) * whiteStrength
